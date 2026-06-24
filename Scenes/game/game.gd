bbb74c7a1c7cd9821f588fc10246044a35708baf
MGARCIA58 <58.mgarcia@gmail.com>
2026-06-23 22:19:15 -0600

    Initial Commit

---
+++ Scenes/game/game.gd
@@ -0,0 +1,46 @@
+extends Node2D
+class_name Game
+
+@onready var player: Player = $Player
+@onready var spawn_pos: Marker2D = $SpawnPos
+@onready var checkpoint_respawn_pos: Marker2D = $checkpointRespawnPos
+@onready var game_won_panel: Panel = %GameWonPanel
+@onready var points_label: Label = %Points
+
+var points: int
+var checkpoint_reached: bool
+
+func _ready() -> void:
+	EventManager.on_player_dead.connect(_on_player_dead)
+	EventManager.on_fruit_collected.connect(_on_fruit_collected)
+	EventManager.on_checkpoint_reached.connect(_on_checkpoint_reached)
+	EventManager.on_game_won.connect(_on_game_won)
+	
+	
+func get_respawn_pos() -> Vector2:
+	if checkpoint_reached:
+		return checkpoint_respawn_pos.position
+	else:
+		return spawn_pos.position
+	
+func _on_player_dead() -> void:
+	player.player_dead()
+	await get_tree().create_timer(0.5).timeout
+	var tween := create_tween()
+	tween.tween_property(player, "global_position", get_respawn_pos(), 0.5)
+	tween.tween_callback(player.player_respawn)
+	
+
+func _on_fruit_collected() -> void:
+	points += 1
+	points_label.text = str(points)
+
+func _on_checkpoint_reached() -> void:
+	checkpoint_reached = true
+	
+func _on_game_won() -> void:
+	game_won_panel.show()
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().reload_current_scene()
---
+++ Scenes/player/player.gd
@@ -0,0 +1,86 @@
+extends CharacterBody2D
+class_name Player
+
+@export var max_speed := 180.0
+@export var jump_force := 450.0
+@export var max_jumps := 2
+@export var gravity := 1600.0
+
+@onready var visuals: Node2D = $Visuals
+@onready var anim_sprite: AnimatedSprite2D = %AnimatedSprite2D
+@onready var ray_cast: RayCast2D = %RayCast2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+var jumps_left : int
+var move_direction := 1
+var can_move := true
+
+func _ready() -> void:
+	jumps_left = max_jumps
+	
+func _physics_process(delta: float) -> void:
+	if not can_move:
+		return
+	handle_movement()
+	handle_gravity(delta)
+	handle_wall_collision()
+	hande_jump_input()
+	move_and_slide()
+	
+func handle_movement() -> void:
+	velocity.x = move_direction * max_speed
+	if is_on_floor():
+		anim_sprite.play("run")
+		jumps_left = max_jumps
+		
+func handle_gravity(delta: float) -> void:
+	if not  is_on_floor():
+		velocity.y += gravity * delta
+		
+func handle_wall_collision() -> void:
+	if not ray_cast.is_colliding():
+		return
+		
+	velocity.y = 50
+	jumps_left = max_jumps
+	anim_sprite.play("fall")
+	
+	if is_on_floor():
+		change_direction()
+		
+func change_direction() -> void:
+	move_direction *= -1
+	visuals.scale.x *= -1
+
+func hande_jump_input() -> void:
+	if not Input.is_action_just_pressed("tap"):
+		return
+	
+	if ray_cast.is_colliding():
+		change_direction()
+		jump()
+	else:
+		jump()
+	
+func jump() -> void:
+	if jumps_left <= 0:
+		return
+	
+	velocity.y = -jump_force
+	jumps_left -= 1
+	if jumps_left <= 0:
+		anim_sprite.play("double_jump")
+	else:
+		anim_sprite.play("jump")
+
+func player_dead() -> void:
+	can_move = false
+	velocity = Vector2.ZERO
+	anim_sprite.play("dead")
+	collision_shape_2d.set_deferred("disabled",true)
+	
+func player_respawn() -> void:
+	anim_sprite.play("respawn")
+	await anim_sprite.animation_finished
+	can_move = true
+	collision_shape_2d.set_deferred("disabled",false)
