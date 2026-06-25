b29934e49fa07dbc2528b6092937d37e742025a4
MGARCIA58 <58.mgarcia@gmail.com>
2026-06-25 12:27:02 -0600

    Se agrega enemico camaleon

---
+++ Scenes/enemies/enemy_chameleon.gd
@@ -0,0 +1,74 @@
+extends CharacterBody2D
+class_name EnemyChameleon
+
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_cast: RayCast2D = $RayCast2D
+@onready var timer: Timer = $Timer
+@export var health: int = 6
+@export var touch_damage = 1
+@export var tongue_damage = 2
+@export var player: Node2D
+@export var body_in_area = false
+
+var defeated := false
+var top_jumped := false
+var bottom_touched := false
+var attacking := false
+var hit_player  := false
+	
+	
+func _process(delta: float) -> void:
+	await get_tree().create_timer(1).timeout
+	attack_player()
+	
+func _on_top_area_body_entered(body: Node2D) -> void:
+	if bottom_touched:
+		return
+	if body is Player:
+		top_jumped = true
+		anim_sprite.play("hit")
+		body.velocity.y = -400
+		health -= 1
+		await anim_sprite.animation_finished
+		#await get_tree().create_timer(0.5).timeout
+		if health <= 0:
+			defeated = true
+			queue_free()
+		top_jumped = false
+	
+func _on_bottom_area_body_entered(body: Node2D) -> void:
+	if defeated or top_jumped:
+			return
+	if body is Player:
+		bottom_touched = true
+		EventManager.on_player_damage.emit(touch_damage)
+		await get_tree().create_timer(2).timeout
+		bottom_touched = false
+	
+	
+func attack_player() -> void:
+	if attacking:
+		return
+	attacking = true
+	hit_player = false
+	anim_sprite.play("attack")
+	await anim_sprite.animation_finished
+	attacking = false
+	
+func _on_animated_sprite_2d_frame_changed() -> void:
+	if anim_sprite == null:
+		return
+	if anim_sprite.animation != "attack":
+		return
+	if anim_sprite.frame == 6:
+		check_tongue_hit()
+	
+func check_tongue_hit() -> void:
+	if hit_player:
+		return
+	ray_cast.force_raycast_update()
+	if ray_cast.is_colliding():
+		var body = ray_cast.get_collider()
+		if body is Player:
+			hit_player = true
+			EventManager.on_player_damage.emit(tongue_damage)
